e5e7c6529b8f1cb59dca818fe46c249f8683cd03
taraqr9 <taraqr9@gmail.com>
2021-05-01 07:13:46 +0600

    Taraqr9

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,20 +18,17 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 
 <body>
     <div id="app">
-        <header>
-            Tweety
-        </header>
+        <section class="px-8">
+            <header>
+                Tweety
+            </header>
+        </section>
 
         <main class="py-4">
             @yield('content')
