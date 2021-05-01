b45e2435a889f37bdca396bae244be848db22c79
taraqr9 <taraqr9@gmail.com>
2021-05-02 01:29:41 +0600

    node js added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -19,12 +19,12 @@
 
     <!-- Styles -->
     
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    
 </head>
 
 <body>
     <div id="app">
-        <section class="px-8">
+        <section class="px-4">
             <header>
                 Tweety
             </header>
